5d3d3b02fde68cc277651dd1713b14c76840f8c1
London <london.rosemary@gmail.com>
2021-10-27 18:17:06 -0400

    Added dream create route

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -6,9 +6,18 @@ class DreamsController < ApplicationController
         render json: @dreams, status: 200
     end
 
+    def create
+        @dream = Dream.create!(dream_params)
+        render json: @dream, status: 200
+    end
+
     private
 
     def render_not_found_response
         render json: { error: "Dream not found" }, status: :not_found
+    end
+
+    def dream_params
+        params.permit(:name, :date, :description, :rating, :mood, :image_url, :user_id)
     end
 end
